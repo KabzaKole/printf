d9e6977bbddb73e5d3ac8c75289aa1245bdb68a8
KabzaKole <kabelomasela47@gmail.com>
2023-03-26 12:42:06 -0700

    get_size.c

---
+++ get_size.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* get_size -Calculates the size to cast the argument
+* @format: Formatted string in which to print the arguments
+* @i: List of arguments to be printed.
+*
+* Return: Size to cast the argument.
+*/
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	/* Check if the next character is a length modifier */
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+    /* Update `i` to the end of the length modifier */
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+    /* Return the size to cast the argument */
+	return (size);
+}
